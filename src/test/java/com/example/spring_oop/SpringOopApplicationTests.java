4ffe8bb6c7cd8686e2aecf2fd048a0357120d3d2
slimshady913 <slimshady913@naver.com>
2022-11-02 02:04:29 +0900

    Initial Commit

---
+++ src/test/java/com/example/spring_oop/SpringOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
