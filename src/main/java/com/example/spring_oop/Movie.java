67065f21d3efc67921df0e169c46172a8d0412e0
slimshady913 <slimshady913@naver.com>
2022-11-23 10:32:35 +0900

    commit2

--- src/main/java/com/example/spring_oop/Movie.java
+++ src/main/java/com/example/spring_oop/Movie.java
@@ -8,9 +8,6 @@ import java.io.Serializable;
 
 @Builder
 @Getter
-public class Movie implements Serializable {
-    private String title;
-    private String link;
-    private float userRating;
+public class Movie {
 }
 
