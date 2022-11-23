3e1c3480faf23dd810464d7de1fce3860ada401c
slimshady913 <slimshady913@naver.com>
2022-11-23 10:38:24 +0900

    MovieService

--- src/main/java/com/example/spring_oop/MovieService.java
+++ src/main/java/com/example/spring_oop/MovieService.java
@@ -2,7 +2,17 @@ package com.example.spring_oop;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 @Service
 public class MovieService {
+    public List<Movie> query (final String query) {
+        return Arrays.asList(
+                Movie.builder().title("영화1").link("http://test").build(),
+                Movie.builder().title("영화2").link("http://test").build()
+        );
+    }
 
 }
