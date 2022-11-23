eb0f5e74c753b53102fdddd31bedd2c2650f3a9c
slimshady913 <slimshady913@naver.com>
2022-11-23 10:29:58 +0900

    commit

--- src/main/java/com/example/spring_oop/SpringOopApplication.java
+++ src/main/java/com/example/spring_oop/SpringOopApplication.java
@@ -1,7 +1,11 @@
 package com.example.spring_oop;
 
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.Serializable;
 
 @SpringBootApplication
 public class SpringOopApplication {
@@ -11,3 +15,4 @@ public class SpringOopApplication {
 	}
 
 }
+
