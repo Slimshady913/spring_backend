84dd39d27bb56d1b07721da8981a58cc45553cc2
Taeseong Choi <slimshady913@naver.com>
2022-12-05 17:33:27 +0900

    views

--- iris/predict/views.py
+++ iris/predict/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import pandas as pd
+from . models import PredResults
 
 def predict(request):
     return render(request, 'predict.html')
@@ -12,3 +15,17 @@ def predict_chances(request):
         sepal_width = float(request.POST.get('sepal_width'))
         petal_length = float(request.POST.get('petal_length'))
         petal_width = float(request.POST.get('petal_width'))
+
+        # Unpickle model
+        model = pd.read_pickle(r"C:\Users\slims\machine\new_model.pickle")
+        # Make prediction
+        result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+
+        classification = result[0]
+
+        PredResults.objects.create(sepal_length=sepal_length, sepal_width=sepal_width, petal_length=petal_length,
+                                   petal_width=petal_width, classification=classification)
+
+        return JsonResponse({'result': classification, 'sepal_length': sepal_length,
+                             'sepal_width': sepal_width, 'petal_length': petal_length, 'petal_width': petal_width},
+                            safe=False)
