528104466f3fe1ec57041a5c7c5bc3b4131c7afa
Taeseong Choi <slimshady913@naver.com>
2022-12-05 17:27:26 +0900

    predict_chances

--- iris/predict/views.py
+++ iris/predict/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def predict(request):
     return render(request, 'predict.html')
+
+def predict_chances(request):
+
+    if request.POST.get('action') == 'post':
+
+        # Receive data from client
+        sepal_length = float(request.POST.get('sepal_length'))
+        sepal_width = float(request.POST.get('sepal_width'))
+        petal_length = float(request.POST.get('petal_length'))
+        petal_width = float(request.POST.get('petal_width'))
