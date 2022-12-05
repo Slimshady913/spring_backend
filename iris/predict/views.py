ef75132f8fe9a986a01032dbb35a722a4980df5e
Taeseong Choi <slimshady913@naver.com>
2022-12-05 21:50:41 +0900

    view result

--- iris/predict/views.py
+++ iris/predict/views.py
@@ -30,3 +30,8 @@ def predict_chances(request):
         return JsonResponse({'result': classification, 'sepal_length': sepal_length,
                              'sepal_width': sepal_width, 'petal_length': petal_length, 'petal_width': petal_width},
                             safe=False)
+
+def view_results(request):
+    # Submit prediction and show all
+    data = {"dataset": PredResults.objects.all()}
+    return render(request, "results.html", data)
